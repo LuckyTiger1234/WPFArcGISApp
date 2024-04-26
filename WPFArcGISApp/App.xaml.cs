a2a1ffa5fbd89609b8b67ceee1d980e838bbcf15
hyt <bdhyt888@163.com>
2024-04-27 00:33:04 +0800

    feat: 引入ArcgisMapSDK，初始化MapView与WebView组件

--- WPFArcGISApp/App.xaml.cs
+++ WPFArcGISApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Configuration;
+﻿using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,6 +11,15 @@ namespace WPFArcGISApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Note: it is not best practice to store API keys in source code.
+            // The API key is referenced here for the convenience of this tutorial.
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKb965a244c5de4ccdbac2c1af7aed9a160S2VFaaMzk2R_1M41OZjD-iKTWrlMFWHaXedZt_CITdfVNwRxZttpo_p1PMeSlsW";
+            var map = new Map(BasemapStyle.ArcGISNavigation);
+
+        }
     }
 
 }
--- WPFArcGISApp/MainWindow.xaml.cs
+++ WPFArcGISApp/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Esri.ArcGISRuntime.Mapping;
+using Esri.ArcGISRuntime.UI.Controls;
 using Microsoft.Web.WebView2.Core;
 
 namespace WPFArcGISApp
@@ -17,11 +19,15 @@ namespace WPFArcGISApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        // arcgis map
+
+        // webview2
         public MainWindow()
         {
             InitializeComponent();
             webView.NavigationStarting += EnsureHttps;
             InitializeAsync();
+            InitBasemap();
         }
         async void InitializeAsync()
         {
@@ -52,6 +58,30 @@ namespace WPFArcGISApp
                 webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
                 args.Cancel = true;
             }
+        }
+
+        private const string TianMapBaseMapUri = "http://t2.tianditu.gov.cn/vec_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=vec&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={level}&TILEROW={row}&TILECOL={col}&tk=fa9bb502614cf3335b92e50f6fe0c371";
+
+        private const string TianMapLabelUri = "http://t2.tianditu.gov.cn/cva_w/wmts?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=cva&STYLE=default&TILEMATRIXSET=w&FORMAT=tiles&TILEMATRIX={level}&TILEROW={row}&TILECOL={col}&tk=fa9bb502614cf3335b92e50f6fe0c371";
+
+        private void InitBasemap()
+        {
+            Esri.ArcGISRuntime.ArcGISRuntimeEnvironment.ApiKey = "AAPKb965a244c5de4ccdbac2c1af7aed9a160S2VFaaMzk2R_1M41OZjD-iKTWrlMFWHaXedZt_CITdfVNwRxZttpo_p1PMeSlsW";
+            var map = new Map(BasemapStyle.ArcGISNavigation);
+            MapView.Map = map;
+            //加载天地图 矢量底图
+            //var webtileBaseLayer = new WebTiledLayer(TianMapBaseMapUri);
+            //MapView.Map.Basemap?.BaseLayers.Add(webtileBaseLayer);
+
+            ////加载天地图 矢量注记
+            //var webtileNodeLayer = new WebTiledLayer(TianMapLabelUri);
+            //MapView.Map.OperationalLayers.Add(webtileNodeLayer);
+
+            MapView.SetViewpoint(new Viewpoint(
+                latitude: 34.2784,
+                longitude: 108.9414,
+                scale: 120000));
+
         }
     }
 }
