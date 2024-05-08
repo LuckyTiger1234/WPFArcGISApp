198f2348a277bd533767e64729a6db208d7cd1e5
hyt <bdhyt888@163.com>
2024-05-09 01:55:52 +0800

    feat: 实现多点勾画；优化插值逻辑为固定距离插值；实现webview控件向VM层传值

--- ViewModel/SceneViewModel.cs
+++ ViewModel/SceneViewModel.cs
@@ -30,15 +30,20 @@ namespace WPFArcGISApp.ViewModel
         private TextBlock _loadingText;
 
         // 创建图层，保存Polyline Graphic
-        private GraphicsOverlay LineOverlay;
-        private GraphicsOverlay PointOverlay;
-        private MapPoint _startPoint;
-        private MapPoint _endPoint;
+        private GraphicsOverlay _LineOverlay;
+        private GraphicsOverlay _PointOverlay;
+        private List<MapPoint> _clickedPoints = new List<MapPoint>();
+
+        // 差值点
+        private List<MapPoint> _interpolatePoints = new List<MapPoint>();
+        private List<double> _elevations = new List<double>();
+        private List<int> _distances = new List<int>();
 
         // 通过该变量控制绘制状态
         private bool _isDrawingLine = false;
 
         private string _buttonContent = "剖面分析工具";
+        
         // 绑定button content
         public string ButtonContent
         {
@@ -52,10 +57,8 @@ namespace WPFArcGISApp.ViewModel
                 }
             }
         }
-
         // 绑定按钮点击事件
         public ICommand DrawLineCommand { get; private set; }
-        public ICommand ClearLineCommand { get; private set; }
 
         public SceneViewModel(
             SceneView sceneView, 
@@ -71,13 +74,18 @@ namespace WPFArcGISApp.ViewModel
             _loadingBar = progressBar;
             _loadingText = textBlock;
 
-            LineOverlay = new GraphicsOverlay();
-            PointOverlay = new GraphicsOverlay();
+            _LineOverlay = new GraphicsOverlay();
+            _PointOverlay = new GraphicsOverlay();
+
+            // 添加点图层到场景视图
+            _sceneView.GraphicsOverlays.Add(_PointOverlay);
+            _sceneView.GraphicsOverlays.Add(_LineOverlay);
+
             // 绑定butoon事件
             DrawLineCommand = new RelayCommand(DrawLine);
             // 绑定对球操作事件
             _sceneView.MouseUp += OnSceneViewMouseUp;
-
+            _sceneView.MouseDoubleClick += OnSceneViewDoubleClick;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
@@ -91,15 +99,14 @@ namespace WPFArcGISApp.ViewModel
             if (ButtonContent == "清除") 
             {
                 // 清除
-                _sceneView.GraphicsOverlays.Remove(LineOverlay);
-                _sceneView.GraphicsOverlays.Remove(PointOverlay);
+                _PointOverlay.Graphics.Clear();
+                _LineOverlay.Graphics.Clear();
+                _clickedPoints.Clear();
+                _interpolatePoints.Clear();
+                _distances.Clear();
+                _elevations.Clear();
 
-                // 重置状态变量和起点终点
                 _isDrawingLine = false;
-                _startPoint = null;
-                _endPoint = null;
-                LineOverlay = new GraphicsOverlay();
-                PointOverlay = new GraphicsOverlay();
                 _webView.Visibility = Visibility.Collapsed;
                 ButtonContent = "剖面分析工具";
             }
@@ -108,7 +115,6 @@ namespace WPFArcGISApp.ViewModel
                 // 绘制
                 _isDrawingLine = true;
             }
-
         }
 
 
@@ -116,90 +122,93 @@ namespace WPFArcGISApp.ViewModel
         {
             if (!_isDrawingLine)
                 return;
-            
             // 获取鼠标点击的地理坐标点
             MapPoint clickedPoint = await _sceneView.ScreenToLocationAsync(e.GetPosition(_sceneView));
             SimpleMarkerSymbol PointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.Blue, 8);
 
-            if (_startPoint == null)
+            // 添加到图层中
+            Graphic pointGraphic = new Graphic(clickedPoint, PointSymbol);
+            _PointOverlay.Graphics.Add(pointGraphic);
+            // 将点击的点添加到列表中
+            _clickedPoints.Add(clickedPoint);
+        }
+
+        private async void OnSceneViewDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // 获取鼠标点击的地理坐标点
+            MapPoint clickedPoint = await _sceneView.ScreenToLocationAsync(e.GetPosition(_sceneView));
+            SimpleMarkerSymbol PointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbolStyle.Circle, Color.Blue, 8);
+
+            // 添加到图层中
+            Graphic pointGraphic = new Graphic(clickedPoint, PointSymbol);
+            _PointOverlay.Graphics.Add(pointGraphic);
+
+            // 将点击的点添加到列表中
+            _clickedPoints.Add(clickedPoint);
+
+            if (_clickedPoints.Count < 2)
             {
-                // 第一次点击，设置起点
-                _startPoint = clickedPoint;
-
-                // 添加到图层中
-                Graphic startPointGraphic = new Graphic(_startPoint, PointSymbol);
-                PointOverlay.Graphics.Add(startPointGraphic);
-                _sceneView.GraphicsOverlays.Add(PointOverlay);
+                MessageBox.Show("至少需要两个点才能绘制折线。");
+                return;
             }
-            else
-            {
-                // 第二次点击，设置终点，并绘制直线
-                _endPoint = clickedPoint;
-                // 添加到图层中
-                Graphic endPointGraphic = new Graphic(_endPoint, PointSymbol);
-                PointOverlay.Graphics.Add(endPointGraphic);
 
-                // 绘制线条
-                PolylineBuilder polylineBuilder = new PolylineBuilder(SpatialReferences.Wgs84);
-                polylineBuilder.AddPoint(_startPoint);
-                polylineBuilder.AddPoint(_endPoint);
-                // 创建 Polyline
-                Polyline polyline = polylineBuilder.ToGeometry();
+            // 将线条添加到图层中
+            SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.Red, 2);
+            Polyline line = new Polyline(_clickedPoints);
+            Graphic polylineGraphic = new Graphic(line, lineSymbol);
 
-                // 获取线的剖面信息(暂定差值100个点)
-                getElevationfromLine(polyline, 100);
+            _LineOverlay.Graphics.Add(polylineGraphic);
 
-                // 将线条添加到图层中
-                SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbolStyle.Solid, Color.Red, 2);
-                Graphic lineGraphic = new Graphic(polyline, lineSymbol);
+            _isDrawingLine = false;
+            ButtonContent = "清除";
 
-                LineOverlay.Graphics.Add(lineGraphic);
-                _sceneView.GraphicsOverlays.Add(LineOverlay);
+            drawChart(line, 10);
 
-                _isDrawingLine = false;
-                ButtonContent = "清除";
-            }
         }
 
         /// <summary>
-        /// 对polyline直线按照线性插值，取线上的点的高程
+        /// 对polyline直线按照线性插值，取线上的点的高程,数据发送到webview控件，绘制折线图
         /// </summary>
         /// <param name="line">polyline 线</param>
-        /// <param name="pointsNum">int 插值数量</param>
-        private async void getElevationfromLine(Polyline line, int pointsNum)
+        /// <param name="stepLen">int 差值步长（m）</param>
+        private async void drawChart(Polyline line, int stepLen)
         {
-            List<double> elevations = new List<double>();
-            List<int> distances = new List<int>();
-
-            // 计算插值步长
-            double step = line.Length() / (pointsNum - 1);
-
             _loadingBar.Visibility = Visibility.Visible;
             _loadingText.Visibility = Visibility.Visible;
 
-            for (int i=0; i < pointsNum; i++)
+
+
+            // 获取polyline长度, 根据步长确定差值数量
+            double length = GeometryEngine.LengthGeodetic(line, linearUnit: LinearUnits.Meters, geodeticCurveType: GeodeticCurveType.Geodesic);
+
+            int interpolateNum =(int)length / stepLen;
+
+            // 计算插值步长
+            double step = line.Length() / (interpolateNum - 1);
+            for (int i = 0; i < interpolateNum; i++)
             {
                 // 计算当前插值点的位置
                 double distance = step * i;
                 MapPoint interpolatedPoint = GeometryEngine.Project(
                     GeometryEngine.CreatePointAlong(line, distance),
                     SpatialReferences.Wgs84) as MapPoint;
-                
+
                 // 获取插值点的高程信息
                 double elevation = await _surface.GetElevationAsync(interpolatedPoint);
-                GeodeticDistanceResult distanceResult = GeometryEngine.DistanceGeodetic(_startPoint, interpolatedPoint, null, null, GeodeticCurveType.Geodesic);
-                int distanceInt = (int) distanceResult.Distance;
+                GeodeticDistanceResult distanceResult = GeometryEngine.DistanceGeodetic(_clickedPoints[0], interpolatedPoint, null, null, GeodeticCurveType.Geodesic);
+                int distanceInt = (int)distanceResult.Distance;
                 elevation = Math.Round(elevation, 2);
-                elevations.Add(elevation);
-                distances.Add(distanceInt);
+
+                _interpolatePoints.Add(interpolatedPoint);
+                _elevations.Add(elevation);
+                _distances.Add(distanceInt);
             }
-            Console.WriteLine("\"length\"",elevations.Count);
 
-            // 计算统计数据
-
+            
+               
             // 将海拔数据传递给 WebView2，并绘制折线图
-            String script = $"drawElevationChart({JsonConvert.SerializeObject(elevations)},{JsonConvert.SerializeObject(distances)} );" +
-                $"updateStatics({elevations.Max()},{elevations.Min()},{Math.Round(elevations.Average())},{Math.Round(CalculateGain(elevations), 2)},{Math.Round(CalculateLoss(elevations), 2)})";
+            String script = $"drawElevationChart({JsonConvert.SerializeObject(_elevations)},{JsonConvert.SerializeObject(_distances)} );" +
+                $"updateStatics({_elevations.Max()},{_elevations.Min()},{Math.Round(_elevations.Average())},{Math.Round(CalculateGain(_elevations), 2)},{Math.Round(CalculateLoss(_elevations), 2)})";
             await _webView.ExecuteScriptAsync(script);
 
             _webView.Visibility = Visibility.Visible;
--- ViewModel/WebViewModel.cs
+++ ViewModel/WebViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Web.WebView2.Wpf;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.Wpf;
 using System;
 using System.IO;
 
@@ -11,6 +12,22 @@ namespace WPFArcGISApp.ViewModel
         public WebViewModel(WebView2 webView)
         {
             _webView = webView;
+            InitializeAsync();
+        }
+
+        async void InitializeAsync()
+        {
+            await _webView.EnsureCoreWebView2Async(null);
+            // 在webview控件增加获取点信息的监听
+            _webView.CoreWebView2.WebMessageReceived += getLinePoint;
+            // 注册 JavaScript 交互对象
+            //await _webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.external = window");
+        }
+
+        void getLinePoint(object sender, CoreWebView2WebMessageReceivedEventArgs e)
+        {
+            String linePointIndex = e.TryGetWebMessageAsString();
+            Console.WriteLine("\"linePointStr\"", linePointIndex);
 
         }
     }
